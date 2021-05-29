f673ff646b64ff3be65a8eeac5210e7b8373bff6
Matheus <ma-the-us39@hotmail.com>
2021-05-29 19:33:26 -0300

    Implementação de RestricoesTest

--- src/test/java/com/sicredi/restricoes/RestricoesTest.java
+++ src/test/java/com/sicredi/restricoes/RestricoesTest.java
@@ -5,17 +5,34 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+
 public class RestricoesTest {
     @Test
     public void testDadoUmCPFComRestricaoEntaoRetornaStatusCode200(){
-      // Configurar o caminho comum de acesso a API REST
-      baseURI = "http://localhost";
-      port = 8080;
-      basePath = "/api";
+        // Configurar o caminho comum de acesso a API REST
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
 
-
-
-
+      //Consultar CPF
+     given()
+             .when()
+                    .get("/v1/restricoes/97093236014")
+             .then()
+                .assertThat()
+                    .statusCode(200);
     }
+    @Test
+    public void testDadoUmCPFSemRestricaoEntaoRetornaStatusCode204(){
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
 
+        given()
+                .when()
+                        .get("/v1/restricoes/12024991637")
+                .then()
+                    .assertThat()
+                        .statusCode(204);
+    }
 }
