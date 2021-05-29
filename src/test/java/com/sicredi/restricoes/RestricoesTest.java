cbb14902f5f03291f088f47d0f51de13f0f9bcfe
Matheus <ma-the-us39@hotmail.com>
2021-05-29 18:53:15 -0300

    Acesso a API

--- src/test/java/com/sicredi/restricoes/RestricoesTest.java
+++ src/test/java/com/sicredi/restricoes/RestricoesTest.java
@@ -1,10 +1,20 @@
 package com.sicredi.restricoes;
 
 import org.junit.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 public class RestricoesTest {
     @Test
     public void testDadoUmCPFComRestricaoEntaoRetornaStatusCode200(){
+      // Configurar o caminho comum de acesso a API REST
+      baseURI = "http://localhost";
+      port = 8080;
+      basePath = "/api";
+
+
+
 
     }
 
