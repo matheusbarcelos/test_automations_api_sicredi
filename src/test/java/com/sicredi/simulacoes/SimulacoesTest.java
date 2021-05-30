af2dbb8b77974dea1bdf0feb81ca9f4531b4d1eb
Matheus <ma-the-us39@hotmail.com>
2021-05-30 06:48:49 -0300

    Implementação SimulacoesTest

--- src/test/java/com/sicredi/simulacoes/SimulacoesTest.java
+++ src/test/java/com/sicredi/simulacoes/SimulacoesTest.java
@@ -9,14 +9,15 @@ import static org.hamcrest.Matchers.*;
 
 public class SimulacoesTest {
     @Before
-    public void setup(){
-            // Configurar o caminho comum de acesso a API REST
-            baseURI = "http://localhost";
-            port = 8080;
-            basePath = "/api";
-        }
+    public void setup() {
+        // Configurar o caminho comum de acesso a API REST
+        baseURI = "http://localhost";
+        port = 8080;
+        basePath = "/api";
+    }
+
     @Test
-    public void testDadoUmCadastroInsereUmaSimulacaoEntaoRetornaStatusCode201(){
+    public void testDadoUmCadastroInsereUmaSimulacaoEntaoRetornaStatusCode201() {
         given()
                 .body("{\n" +
                         "  \"nome\": \"Shaka\",\n" +
@@ -28,14 +29,53 @@ public class SimulacoesTest {
                         "}")
                 .contentType(ContentType.JSON)
                 .when()
-                    .post("/v1/simulacoes")
-                    .then()
-                    .assertThat()
-                    .statusCode(201);
+                .post("/v1/simulacoes")
+                .then()
+                .assertThat()
+                .statusCode(201);
     }
 
     @Test
-    public void testRetornarSimulacoesExistentes(){
+    public void testDadoUmCadastroIncompletoAoInserirUmaSimulacaoEntaoRetornaStatusCode400() {
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Shaka\",\n" +
+                        "  \"cpf\": 97093236014,\n" +
+                        "  \"email\": \"shaka@gmail.com\",\n" +
+                        "  \"valor\": 1200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .assertThat()
+                .statusCode(409);
+    }
+
+    @Test
+    public void testDadoUmCadastroExistenteAoInserirUmaSimulacaoEntaoRetornaStatusCode409() {
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Matheus\",\n" +
+                        "  \"cpf\": 12024991637,\n" +
+                        "  \"email\": \"shaka@gmail.com\",\n" +
+                        "  \"valor\": 1200,\n" +
+                        "  \"parcelas\": 3,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/v1/simulacoes")
+                .then()
+                .assertThat()
+                .statusCode(409);
+    }
+
+
+    @Test
+    public void testRetornarSimulacoesCadastradas() {
         given()
                 .when()
                 .get("/v1/simulacoes/")
@@ -45,5 +85,57 @@ public class SimulacoesTest {
                 .statusCode(200);
     }
 
-
+    @Test
+    public void testDadoUmCPFAltereUmaSimulacaoExistenteEntaoRetornaStatusCode200() {
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Fulano\",\n" +
+                        "  \"cpf\": 66414919004,\n" +
+                        "  \"email\": \"teste@gmail.com\",\n" +
+                        "  \"valor\": 5000,\n" +
+                        "  \"parcelas\": 5,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .put("/v1/simulacoes/66414919004")
+                .then()
+                .assertThat()
+                .statusCode(200);
     }
+
+    @Test
+    public void testDadoUmCPFInexistenteEntaoRetornaStatusCode404() {
+        given()
+                .body("{\n" +
+                        "  \"nome\": \"Fulano\",\n" +
+                        "  \"cpf\": 12024991636,\n" +
+                        "  \"email\": \"teste@gmail.com\",\n" +
+                        "  \"valor\": 5000,\n" +
+                        "  \"parcelas\": 5,\n" +
+                        "  \"seguro\": true\n" +
+                        "}")
+                .contentType(ContentType.JSON)
+                .when()
+                .put("/v1/simulacoes/12024991636")
+                .then()
+                .assertThat()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testDadoUmCPFConsultaSuaSituacaoEntaoRetornaStatusCode200(){
+        given()
+                .when()
+                .get("/v1/simulacoes/66414919004")
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200);
+    }
+
+}
+
+
+
+
